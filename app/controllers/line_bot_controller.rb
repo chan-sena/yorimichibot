1989c7d3fb1595001b85e88d1d1c91fe7b87a854
chan-sena <senaakaneiro1020@gmail.com>
2022-11-25 21:23:00 +0900

    11/25

--- app/controllers/line_bot_controller.rb
+++ app/controllers/line_bot_controller.rb
@@ -53,12 +53,18 @@ class LineBotController < ApplicationController
     def transfer_create_message(departure, destination)
       # 外部APIへGETリクエストするためのライブラリをインスタンス化、このhttp_clientでgetメソッドを使うと指定したURLに対してGETリクエストを行いそのレスポンスを取得できる
       http_client = HTTPClient.new
+      # mechanizeのライブラリをインスタンス化してagent変数に代入
       agent = Mechanize.new
+      # agent変数にURLに対してgetリクエストを行い、その結果をpage変数に代入
       page = agent.get('https://transit.yahoo.co.jp/search/print?from=#{departure}&flation=&to=#{destination}')
       # page = agent.get('https://transit.yahoo.co.jp/search/result?from=%E6%B1%9F%E5%8F%A4%E7%94%B0&to=%E6%96%B0%E5%AE%BF&fromgid=&togid=&flatlon=&tlatlon=&y=2022&m=11&d=07&hh=17&m1=2&m2=0&type=1&ticket=ic&expkind=1&userpass=1&ws=3&s=0&al=1&shin=1&ex=1&hb=1&lb=1&sr=1')
+      # elements変数にpage変数で取得したページから該当のタグの文章を抽出
       elements = page.search('#route03 .station dl dt').inner_text
+      # elements変数を表示
       puts elements
+      # getメソッドを使用しGETリクエストを送信
       response = http_client.get(page, elements)
+      # GETリクエストに対するレスポンスをrespon
       response = JSON.parse(response.body)
     end
 end
