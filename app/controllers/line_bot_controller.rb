bc1f2059150b4e359f7113f451b168901781ad05
chan-sena <senaakaneiro1020@gmail.com>
2023-02-26 23:42:19 +0900

    flexメッセージ実装2/26

--- app/controllers/line_bot_controller.rb
+++ app/controllers/line_bot_controller.rb
@@ -57,14 +57,23 @@ class LineBotController < ApplicationController
             text = event.message['text']
             results = search_restaurants(text)
             if results.present?
-              reply_text = results.map{|result| "#{result['name']}は#{result['genre']['name']}のお店です。" + "\n" + "住所は#{result['address']}です。"}.join("\n")
+              message = {
+                type: 'flex',
+                altText: 'よりみちできるグルメスポットの検索結果です！',
+                contents: restaurants_bubble(results)
+              }
+              # reply_text = results.map{|result| "☆#{result['name']}"+ "\n" + "ジャンル：#{result['genre']['name']}" + "\n" + "住所：#{result['address']}"+ "\n" + "営業日時：#{result['open']}"+ "\n" + "休日：#{result['close']}"+ "\n" + "URL：#{result['urls']['pc']}"}.join("\n")
             else
               reply_text = "「#{text}」に該当するお店は見つかりませんでした。"
+              message = {
+                type: 'text',
+                text: reply_text
+              }
             end
-            message ={
-              type: 'text',
-              text: 'よりみちできるグルメスポットの検索結果です！' + "\n" + "\n" +  reply_text + "\n" + "\n" + 'Powered by ホットペッパー Webサービス'
-            }
+            # message ={
+            #   type: 'text',
+            #   text: 'よりみちできるグルメスポットの検索結果です！' + "\n" + "\n" +  reply_text + "\n" + "\n" + 'Powered by ホットペッパー Webサービス'
+            # }
           else
             message = {
               type: 'text',
@@ -246,6 +255,174 @@ class LineBotController < ApplicationController
       })
       res = Net::HTTP.get_response(uri)
       JSON.parse(res.body)['results']['shop']
+    end
+
+
+    def restaurants_bubble(shops)
+      bubbles = []
+      shops.map do |shop|
+      bubble =
+      {
+        type: "bubble",
+        hero: {
+          type: "image",
+          url: "#{shop['photo']['mobile']['l']}",
+          size: "full",
+          aspectRatio: "20:13",
+          aspectMode: "cover",
+          action: {
+            type: "uri",
+            uri: shop['urls']['pc']
+          }
+        },
+        body: {
+          type: "box",
+          layout: "vertical",
+          contents: [
+            {
+              type: "text",
+              text: shop['name'],
+              weight: "bold",
+              size: "xl",
+              wrap: true
+            },
+            {
+              type: "box",
+              layout: "vertical",
+              margin: "lg",
+              spacing: "sm",
+              contents: [
+               {
+                  type: "box",
+                  layout: "baseline",
+                  contents: [
+                    {
+                      type: "text",
+                      text: "カテゴリ",
+                      flex: 1,
+                      size: "sm",
+                      color: "#aaaaaa",
+                      wrap: true
+                    },
+                    {
+                     type: "text",
+                     text: shop['genre']['name'],
+                     flex: 3,
+                     size: "sm",
+                     color: "#666666"
+                    }
+                  ]
+               },
+               {
+                  type: "box",
+                  layout: "baseline",
+                  spacing: "sm",
+                  contents: [
+                    {
+                      type: "text",
+                      text: "住所",
+                      color: "#aaaaaa",
+                      size: "sm",
+                      flex: 1
+                    },
+                    {
+                      type: "text",
+                      wrap: true,
+                      color: "#666666",
+                      size: "sm",
+                      flex: 5,
+                      text: shop['address']
+                    }
+                  ]
+                },
+                {
+                  type: "box",
+                  layout: "baseline",
+                  spacing: "sm",
+                  contents: [
+                    {
+                      type: "text",
+                      text: "営業日時",
+                      color: "#aaaaaa",
+                      size: "sm",
+                      flex: 1
+                    },
+                    {
+                      type: "text",
+                      color: "#666666",
+                      size: "sm",
+                      flex: 3,
+                      text: shop['open']
+                    }
+                  ]
+                },
+                {
+                  type: "box",
+                  layout: "baseline",
+                  contents: [
+                    {
+                      type: "text",
+                      text: "休日",
+                      flex: 1,
+                      color: "#aaaaaa",
+                      size: "sm"
+                    },
+                    {
+                      type: "text",
+                      text: shop['close'],
+                      flex: 3,
+                      size: "sm",
+                      color: "#666666"
+                    }
+                  ]
+                },
+                {
+                  type: "box",
+                  layout: "baseline",
+                  contents: [
+                    {
+                      type: "text",
+                      action: {
+                        type: "uri",
+                        label: "action",
+                        uri: "http://webservice.recruit.co.jp/"
+                      },
+                      text: "Powered by ホットペッパー Webサービス",
+                      wrap: true,
+                      size: "xs",
+                      color: "#aaaaaa"
+                    }
+                  ]
+                }
+              ]
+            }
+          ]
+        },
+        footer: {
+          type: "box",
+          layout: "vertical",
+          spacing: "sm",
+          contents: [
+            {
+              type: "button",
+              style: "link",
+              height: "sm",
+              action: {
+                type: "uri",
+                label: "詳細はこちら",
+                uri: shop['urls']['pc']
+              }
+            }
+          ],
+          flex: 0
+         }
+      }
+      bubbles.push bubble
+    end
+      {
+        type: 'carousel',
+        contents: bubbles
+      }
     end
     # return results
       # # getメソッドを使用しGETリクエストを送信
